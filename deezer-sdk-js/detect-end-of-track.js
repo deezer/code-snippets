d3c5f72120e2251f1d9200b3acba036b42f48a51
Jean Lecordier <47030586+jlecordier@users.noreply.github.com>
2021-09-18 17:09:31 +0200

    clean code

--- deezer-sdk-js/detect-end-of-track.js
+++ deezer-sdk-js/detect-end-of-track.js
@@ -1,19 +1,19 @@
-var old_track, current_track;
- 
-DZ.Event.subscribe('current_track', function(data, evt_name) {
-  
-  // Set var current_track with the current track id
-  current_track = data.track.id;
- 
-  if (old_track != undefined && current_track != old_track) {
+let oldTrackId, currentTrackId;
+
+DZ.Event.subscribe('current_track', (data, eventName) => {
+
+  // Set var currentTrackId with the current track id
+  currentTrackId = data.track.id;
+
+  if (oldTrackId && currentTrackId !== oldTrackId) {
     // Old track is complete, new track is loaded  
     // Do something here...
   }
- 
+
   // save old track id
-  old_track  = current_track;
- 
+  oldTrackId = currentTrackId;
+
   // change document title
-  document.title = data.track.title + " by "+ data.track.artist.name;
- 
+  document.title = `${data.track.title} by ${data.track.artist.name}`;
+
 });
